62b1e8533de8efeb641bd3ec23f6e00480b5c80b
Özgür İnce <ozgur@ince.ist>
2024-04-21 10:07:51 +0300

    install mssql, show rez counts in dahsboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import { getRezCounts } from '@/app/lib/data';
+
+export default async function Page() {
+  const { Waiting, Processing, Canceled, Transferred } = await getRezCounts();
+
+  return (
+    <main>
+      <h1 className="mb-4 text-xl md:text-2xl">Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Waiting" value={Waiting} type="waiting" />
+        <Card title="Processing" value={Processing} type="processing" />
+        <Card title="Canceled" value={Canceled} type="canceled" />
+        <Card title="Transferred" value={Transferred} type="transferred" />
+      </div>
+    </main>
+  );
 }
---
+++ app/lib/data.ts
@@ -0,0 +1,41 @@
+import mssql from 'mssql';
+
+const sqlConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  server: process.env.DB_SERVER ?? 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    trustServerCertificate: true,
+    cryptoCredentialsDetails: {
+      ciphers: 'DEFAULT@SECLEVEL=0',
+    },
+  },
+};
+
+interface RezCounts {
+  Waiting: number;
+  Processing: number;
+  Canceled: number;
+  Transferred: number;
+}
+
+export async function getRezCounts(): Promise<RezCounts> {
+  try {
+    await mssql.connect(sqlConfig);
+    const result = await mssql.query`
+      select SUM(IIF(RezDurum = 0, 1, 0)) Waiting, SUM(IIF(RezDurum = 1, 1, 0)) Processing,
+             SUM(IIF(RezDurum = 2, 1, 0)) Canceled, SUM(IIF(RezDurum = 3, 1, 0)) Transferred
+        from Web_Rezervasyon;`;
+    console.log('Database Result:', result.recordset[0]);
+    return result.recordset[0];
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch card data.');
+  }
+}
