c7c1eafcd6cef10324f36fca0d42986e232094aa
Özgür İnce <ozgur@ince.ist>
2024-04-25 13:00:09 +0300

    modify layout

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -27,7 +27,7 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex justify-center md:justify-start md:px-3 h-[48px] grow items-center gap-2 rounded-md bg-gray-50 text-sm font-medium hover:bg-sky-100 hover:text-blue-600',
               { 'bg-sky-100 text-blue-600': pathname === link.href }
             )}
           >
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
@@ -7,9 +6,12 @@ export default function SideNav() {
     <div className="flex flex-row md:flex-col">
       <NavLinks />
       <form>
-        <button type="button">
-          <PowerIcon />
-          <div className="hidden md:block">Sign Out</div>
+        <button
+          type="button"
+          className="flex justify-center md:justify-start md:px-3 h-[48px] w-full grow items-center gap-2 rounded-md bg-gray-50 text-sm font-medium hover:bg-sky-100 hover:text-blue-600"
+        >
+          <PowerIcon className="w-6" />
+          <p className="hidden md:block">Sign Out</p>
         </button>
       </form>
     </div>
