6fc49636ed43cc48849945d077fa64bb4c25f994
Özgür İnce <ozgur@ince.ist>
2024-04-25 11:11:01 +0300

    modify layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64 h-screen">
+    <div>
+      <div>
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12 h-screen">{children}</div>
+      <div>{children}</div>
     </div>
   );
 }
--- app/dashboard/reservations/page.tsx
+++ app/dashboard/reservations/page.tsx
@@ -16,18 +16,17 @@ export default async function Page({
   const currentPage = Number(searchParams?.page) || 1;
 
   return (
-    <div className="w-full h-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl">Reservations</h1>
+    <div>
+      <div>
+        <h1>Reservations</h1>
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+      <div>
         <Search placeholder="Search reservations..." />
         <CreateReservation />
       </div>
       <Suspense key={query + currentPage} fallback={<ReservationsTableSkeleton />}>
         <TableWrapper query={query} currentPage={currentPage} />
       </Suspense>
-      <div className="mt-5 flex w-full justify-center" />
     </div>
   );
 }
